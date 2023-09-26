8a6566c0f5600d035cf30e6a1e586204abf58c0d
sunjiaqing <sundada214@outlook.com>
2023-09-26 10:54:29 +0800

    1111

--- httpclient-spring-boot-samples/src/main/java/com/github/shy526/samples/SampleMapperApplication.java
+++ httpclient-spring-boot-samples/src/main/java/com/github/shy526/samples/SampleMapperApplication.java
@@ -2,24 +2,12 @@ package com.github.shy526.samples;
 
 
 import com.github.shy526.http.HttpClientService;
-import com.github.shy526.http.HttpResult;
-import jdk.nashorn.api.scripting.ScriptObjectMirror;
 import lombok.extern.slf4j.Slf4j;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.util.StringUtils;
-
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author shy526
